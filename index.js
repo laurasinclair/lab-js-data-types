c0a9f841206cf07a28bc2098037c6a5177df7686
Laura Sinclair <l.sinclair@tuta.io>
2024-03-27 16:05:08 +0100

    Solved lab

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
+let tongueTwister = `${s1} ${s2} ${s3} ${s4} ${s5} ${s3} ${s2} ${s1} ${s4}`;
 
 // Print out the concatenated string
-
+console.log(tongueTwister);
 
 
 
@@ -23,10 +23,17 @@ const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
 
+function toCamelTail (word) {
+    const lastLetterUppercase = word[word.length - 1].toUpperCase();
+    const removeEndWord = word.slice(0, word.length - 1);
+    return removeEndWord + lastLetterUppercase
+}
+
+let result = toCamelTail(part1) + toCamelTail(part2);
 
 // Print the cameLtaiL-formatted string
 
-
+console.log(result)
 
 
 /*******************************************
@@ -36,9 +43,11 @@ const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
 
+const tipAmount = billTotal + ((15/100) * billTotal);
 
 // Print out the tipAmount
 
+console.log(tipAmount);
 
 
 
@@ -48,9 +57,11 @@ const billTotal = 84;
 
 // Generate a random integer between 1 and 10 (inclusive)
 
+randomNumber = Math.floor(Math.random() * 10) + 1;
 
 // Print the generated random number
 
+console.log(randomNumber);
 
 
 /*******************************************
@@ -61,16 +72,18 @@ const a = true;
 const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
-const expression1 = a && b;
+const expression1 = a && b; // false
 
-const expression2 = a || b;
+const expression2 = a || b; // true
 
-const expression3 = !a && b;
+const expression3 = !a && b; // false
 
-const expression4 = !(a && b);
+const expression4 = !(a && b); // true
 
-const expression5 = !a || !b;
+const expression5 = !a || !b; // true
 
-const expression6 = !(a || b);
+const expression6 = !(a || b); // false
 
-const expression7 = a && a;
+const expression7 = a && a; // true
+
+console.log(expression1, expression2, expression3, expression4, expression5, expression6, expression7)
