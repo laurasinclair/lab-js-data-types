9b11750e8bb5571e954e75d40881c0e3386eea34
Laura Sinclair <l.sinclair@tuta.io>
2024-03-29 10:40:12 +0100

    done

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ console.log(tipAmount);
 
 // Generate a random integer between 1 and 10 (inclusive)
 
-randomNumber = Math.floor(Math.random() * 10) + 1;
+const randomNumber = Math.floor(Math.random() * 10) + 1;
 
 // Print the generated random number
 
